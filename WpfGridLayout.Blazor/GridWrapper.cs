4ffc0dc73cb9725726aba2e1f0d604150fe0646f
Aurélien BOUDOUX <aurelien@boudoux.fr>
2020-03-27 18:09:17 +0100

    adding Width / Height attributes to the Grid + handling ZIndex

--- WpfGridLayout.Blazor/GridWrapper.cs
+++ WpfGridLayout.Blazor/GridWrapper.cs
@@ -1,13 +1,17 @@
+using System.Globalization;
 using System.Linq;
 
 namespace WpfGridLayout.Blazor
 {
 	public class GridWrapper
 	{
+		private readonly CultureInfo _enCulture = new CultureInfo("en-GB");
 		private readonly GridTemplateConverter _columns = new GridTemplateConverter();
 		private readonly GridTemplateConverter _rows = new GridTemplateConverter();
+		private double? _width;
+		private double? _height;
 
-		public string Css => $"display: grid; {GenerateTemplateColumnsIfAny()}{GenerateTemplateRowsIfAny()}".TrimEnd();
+		public string Css => $"display: grid; {Width()}{Height()}{GenerateTemplateColumnsIfAny()}{GenerateTemplateRowsIfAny()}".TrimEnd();
 
 		private string GenerateTemplateColumnsIfAny() => _columns.Any()
 			? $"grid-template-columns: {string.Join(" ", _columns)}; "
@@ -17,11 +21,17 @@ namespace WpfGridLayout.Blazor
 			? $"grid-template-rows: {string.Join(" ", _rows)};"
 			: string.Empty;
 
+		private string Width() => $"{(_width.HasValue ? "width:" + _width.Value.ToString(_enCulture) + "px;" : string.Empty)}";
+		private string Height() => $"{(_height.HasValue ? "height:"+ _height.Value.ToString(_enCulture) + "px;" : string.Empty)}";
+
 		public void AddColumn(string width, string min = null, string max = null) => _columns.AddData(width, min, max);
 
 		public void AddRow(string height, string min = null, string max = null) => _rows.AddData(height, min, max);
 
-		public string RowGap(int gap) => gap > 0 ? $"grid-row-gap: {gap}px;" : string.Empty;
-		public string ColumnGap(int gap) => gap > 0 ? $"grid-column-gap: {gap}px;" : string.Empty;
+		public void SetWidth(double? value) => _width = value;
+		public void SetHeight(double? value) =>_height = value;
+
+		public string RowGap(double gap) => gap > 0 ? $"grid-row-gap: {gap.ToString(_enCulture)}px;" : string.Empty;
+		public string ColumnGap(double gap) => gap > 0 ? $"grid-column-gap: {gap.ToString(_enCulture)}px;" : string.Empty;
 	}
 }
